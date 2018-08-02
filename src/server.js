f95101958e30b3ff8135a3bb4415a1f8db35cdf2
Antti Tevanlinna <anttitevanlinna@nx233.elisa.fi>
2018-08-02 09:31:24 +0300

    server into module

--- src/server.js
+++ src/server.js
@@ -1,12 +1,18 @@
-var config = require('../../config.json');
+module.exports.server = function(port){
+
+console.log(__dirname);
+
+const config = require('../../config.json');
 
 const express = require('express')
-const app = express()
+const proxy = require('express-http-proxy');
 
-var proxy = require('express-http-proxy');
+const app = express()
 
 app.use('/proxy', proxy(config.jiraserver));
 app.use(express.static('jira-backlog/src/public'));
 app.get('/config', (req, res) => res.send(config));
 
-app.listen(3000, () => console.log('App listening on port 3000!'))
+app.listen(port, () => console.log('App listening on port '+port));
+
+}
